e0ed34ff570fee17c771243eeb621a7cd37393ee
Ankit Budhathoki <ankitbc777@gmail.com>
2024-11-16 22:55:10 +0545

    add all files

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+$('.main-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:false,
+    autoplayTimeout:2500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.special-menu-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
+$('.testimonial-items').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:false,
+    autoplayTimeout:2500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        },
+        1200:{
+            items:1
+        }
+    }
+})
+
+// settings for notice modal
+$('document').ready(function(){
+$('#notice').modal('show')
+})
